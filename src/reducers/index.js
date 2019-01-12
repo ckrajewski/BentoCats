5e53b474dbed749bd1073ef3cfe2448325c5adf9
chris <chris@chriss-MacBook-Air.local>
2019-01-11 23:11:40 -0800

    cleaned API

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,7 @@
 import { combineReducers } from "redux"
-import {CatPicReducer, CatFactReducer} from "./someReducer";
+import {CatFactsAndPicsReducer} from "./someReducer";
 
 export default combineReducers({
-	catPics: CatPicReducer,
-	catFacts: CatFactReducer,
+	catFactsAndPics: CatFactsAndPicsReducer,
 
 })
